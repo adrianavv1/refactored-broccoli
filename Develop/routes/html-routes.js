cf8d6dae52ff0d50f2cf412e5060c57b5bb76a2b
Adrianavv1 <adrianavaldiglesias2@gmail.com>
2021-03-06 23:00:43 -0500

    added to html file to html routes

--- Develop/routes/html-routes.js
+++ Develop/routes/html-routes.js
@@ -5,18 +5,19 @@ const path = require('path');
 // Routes
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
-  // index route loads view.html
-//   router.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../public/blog.html'));
-//   });
-
   router.get('/stats', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/stats.html'));
   });
 
-  // blog route loads blog.html
+  // blog route loads exercise.html
   router.get('/exercise', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/exercise.html'));
   });
+
+  // If no matching route is found default to home
+  router.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+
 
   module.exports = router;
